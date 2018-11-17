7f7a81551ccb3afaca18b79166c00a1decd52e2b
GLasou <36538772+GLasou@users.noreply.github.com>
2018-11-17 22:16:42 +0100

    V3
    Ajout de l'IA se rapprochant du pacman

---
+++ PACMAN-master-V3/entite.h
@@ -0,0 +1,43 @@
+#include "console.h"
+#include <time.h>
+
+//Dimensions du labyrinthe
+#define HAUTEUR 15
+#define LARGEUR 15
+
+#define POSX 5
+#define POSY 5
+
+///Entités pacman (joueur) et fantomes (IA)
+
+typedef struct pacman
+{
+    int pos_x, pos_y; //position de pacman
+    int dir; //direction de pacman
+    int power_up;
+    int vie;
+
+}t_pacman;
+
+typedef struct fantome
+{
+    int pos_x, pos_y;
+    int dir;
+    int vulne;
+    int spawn;
+}t_fantome;
+
+
+void collision_pacman(t_pacman *pac, int mat[LARGEUR][HAUTEUR], int next_dep);
+
+int deplacement_pacman(t_pacman *pac, t_fantome *fan, int next_dep, int laby[LARGEUR][HAUTEUR]);
+
+void collision_fantome(t_fantome *fan, int mat[LARGEUR][HAUTEUR], int next_dep);
+
+int deplacement_fantome(t_fantome *fan, t_pacman *pac, int laby[LARGEUR][HAUTEUR], int bordures);
+
+int deplacement_fantome_lvl3(t_fantome *fan, t_pacman *pac,int laby[LARGEUR][HAUTEUR]);
+
+int mort(t_pacman *pac, t_fantome *fan);
+
+void niveau1(t_pacman* pac);
---
+++ PACMAN-master-V3/terrain.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "entite.h"
+
+int lecture(int mat[LARGEUR][HAUTEUR]/*, int terrain, int nombre_diamants*/);
+
+void affichage(int laby[LARGEUR][HAUTEUR], t_pacman *pac, t_fantome *fan);
